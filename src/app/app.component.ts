a238d245982aa008e445d75adef0cd753567d1ed
JegaDeva <pjegajothi@gmail.com>
2021-10-12 22:28:26 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  serverElements = [
+    { type: 'server', name: 'TestServer', content: 'Just a Test' },
+  ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(bluePrintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'server1',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent,
+    });
+  }
+}
